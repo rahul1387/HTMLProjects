675ba1e40aead64e02689da0c694ce22e664176a
rahul1387 <rahultripathi7447@gmail.com>
2023-11-10 19:56:35 +0530

    Lamp on-off

---
+++ ProjectNo1/script.js
@@ -0,0 +1,8 @@
+let btn = document.getElementById("btn");
+let light = document.getElementById("light");
+
+function toggleBtn()
+{
+    btn.classList.toggle("active");
+    light.classList.toggle("on");   
+}
